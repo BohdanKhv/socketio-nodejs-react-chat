4c21cf9c776c1532f60e7753ece10878955dcf2d
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-02-06 13:08:02 -0500

    Ui fix

--- index.js
+++ index.js
@@ -57,15 +57,15 @@ io.on('connection', (socket) => {
 
 // Serve client react instead of backend 
 // Add the follwing code to your server file on the backend 
-// const path = require('path');
-// if (process.env.NODE_ENV === 'production') {
-//     // Serve any static files
-//     app.use(express.static(path.join(__dirname, 'client/build')));
-//     // Handle React routing, return all requests to React app
-//     app.get('*', function(req, res) {
-//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//     });
-// }
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 server.listen(3001, () => {
     console.log("Server is runnig")
